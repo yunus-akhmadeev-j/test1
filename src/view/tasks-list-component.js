2ebe44a5febad93cb6a37245453d4f20b85e2b56
Yunus Akhmadeev <akuma-sensei@inbox.ru>
2024-09-25 12:39:12 +0300

    Add files via upload

--- src/view/tasks-list-component.js
+++ src/view/tasks-list-component.js
@@ -1,33 +1,40 @@
 import { createElement } from "../framework/render.js";
 
 function createTaskColumnComponentTemplate() {
-  return( ` 
+  return (` 
+  
+  
+
+  <div class="ttt">
   <div class="task-body">
-  <ul class="task-list"><h3>Backlog</h3>
-      <li class="task1">Выучить JS</li>
-      <li class="task1">Выучить React</li>
-      <li class="task1">Сделать домашку</li>
-  </ul>
-</div>
-<div class="task-body">
-  <ul class="task-list"><h3>In progress</h3>
-      <li class="task1">Выпить смузи</li>
-      <li class="task1">Попить воды</li>
-  </ul>
-</div>
-<div class="task-body">
-  <ul class="task-list"><h3>Done</h3>
-      <li class="task1">Позвонить маме</li>
-      <li class="task1">Погладить кота</li>
-  </ul>
-</div>
-<div class="task-body">
-  <ul class="task-list"><h3>Basket</h3>
-      <li class="task1">Сходить погулять</li>
-      <li class="task1">Прочитать Войну и Мир</li>
-      
-  </ul>
-</div>`);
+      <ul class="task-list">Backlog
+          <li class="task1">Выучить JS</li>
+          <li class="task1">Выучить React</li>
+          <li class="task1">Сделать домашку</li>
+      </ul>
+  </div>
+  <div class="task-body">
+      <ul class="task-list">In progress
+          <li class="task1">Выпить смузи</li>
+          <li class="task1">Попить воды</li>
+      </ul>
+  </div>
+  <div class="task-body">
+      <ul class="task-list">Done
+          <li class="task1">Позвонить маме</li>
+          <li class="task1">Погладить кота</li>
+      </ul>
+  </div>
+  <div class="task-body">
+      <ul class="task-list">Basket
+          <li class="task1">Сходить погулять</li>
+          <li class="task1">Прочитать Войну и Мир</li>
+          
+      </ul>
+  </div>
+         </div>   
+
+`);
 }
 
 export default class TaskColumnComponent {
