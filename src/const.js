bc5a4f7a0881931e950da44bcac8274f50260c75
Yunus Akhmadeev <akuma-sensei@inbox.ru>
2024-09-25 20:21:07 +0300

    lab2 update

--- src/const.js
+++ src/const.js
@@ -2,15 +2,29 @@ const Status = {
     BACKLOG: `backlog`,
     PROCESSING: `in-process`,
     DONE: `done`,
-    BASKET: `basket`,
+    BASKET: `in-basket`,
   };
   
 
 
 const StatusLabel = {
-    [Status.BACKLOG]: `Бэклог`,
-    [Status.PROCESSING]: `В процессе`,
-    [Status.DONE]: `Готово`,
-    [Status.BASKET]: `Корзина`,
+    [Status.BACKLOG]: `Backlog`,
+    [Status.PROCESSING]: `In Process`,
+    [Status.DONE]: `Done`,
+    [Status.BASKET]: `Basket`,
   };
-export {Status, StatusLabel};
+
+  const title = {
+    title: `Выучить Js`,
+    title: `Выучить React`,
+    title: `Сделать домашку`,
+    title: `Выпить смузи`,
+    title: `Попить воды`,
+     title: `Позвонить маме`,
+     title: `Погладить кота`,
+     title: `Сходить погулять`,
+    title: `Прочитать Войну и Мир`,
+
+
+  }
+export {Status, StatusLabel, title};
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import HeaderComponent from "./view/header.js";
 import FormAddTaskComponent from "./view/task-adder.js";
-import TaskColumnComponent  from "./view/tasks-list-component.js";
-import TaskComponent from "./view/task-component.js";
-import { render, RenderPosition } from "./framework/render.js";
+import TaskBoardComponent  from "./view/tasks-board.js";
 import TaskBasket from "./view/task-basket.js";
+import { render, RenderPosition } from "./framework/render.js";
+
 
 
 
 const bodyContainer = document.querySelector("body");
 const formContainer = document.querySelector(".newtask-add-button");
-const taskcolumnContainer = document.querySelector(".tasks");
+const taskboardContainer = document.querySelector(".tasks");
 const basketContainer = document.querySelector(".clear-button");
 
 
@@ -17,8 +17,7 @@ const basketContainer = document.querySelector(".clear-button");
 
 render(new HeaderComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
 render(new FormAddTaskComponent(), formContainer);
-render(new TaskColumnComponent(), taskcolumnContainer);
+render(new TaskBoardComponent(), taskboardContainer);
 render(new TaskBasket(), basketContainer);
-
 
 
