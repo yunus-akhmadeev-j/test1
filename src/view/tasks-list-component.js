13dcfbd6b72b840a9227385d7762fb9de76549f1
Yunus Akhmadeev <akuma-sensei@inbox.ru>
2024-09-25 12:03:56 +0300

    Update tasks-list-component.js

--- src/view/tasks-list-component.js
+++ src/view/tasks-list-component.js
@@ -1,7 +1,7 @@
 import { createElement } from "../framework/render.js";
 
 function createTaskColumnComponentTemplate() {
-  return ` 
+  return( ` 
   <div class="task-body">
   <ul class="task-list"><h3>Backlog</h3>
       <li class="task1">Выучить JS</li>
@@ -27,7 +27,7 @@ function createTaskColumnComponentTemplate() {
       <li class="task1">Прочитать Войну и Мир</li>
       
   </ul>
-</div>`;
+</div>`;)
 }
 
 export default class TaskColumnComponent {
